cb4208b9371cc1888bec1f7b4c8beec79cdcb011
dingxuewu <2497436726@qq.com>
2021-09-15 15:25:29 +0800

    getCurrentInstance

--- Vue3/src/Index.vue
+++ Vue3/src/Index.vue
@@ -4,17 +4,19 @@
 v-model:name='name'
 v-model:age='age'
 ></v-model> -->
-<watch/>
+<!-- <watch/> -->
+<get-current-instance/>
 </template>
 
 <script>
 import { reactive, toRefs } from "@vue/reactivity"
 // import VModel from './components/V-model.vue'
-import Watch from './components/Watch.vue'
+// import Watch from './components/Watch.vue'
+import GetCurrentInstance from './components/GetCurrentInstance.vue'
 
 export default {
   name: 'Index',
-  components: { Watch },
+  components: { GetCurrentInstance },
   setup () {
     const state = reactive({
       name: '橙子',
---
+++ Vue3/src/components/GetCurrentInstance.vue
@@ -0,0 +1,39 @@
+// setup中如何获取组件实例
+/*
+1. 在setup和其他Composition API中没有this
+
+2. 可通过getCuurentInstance获取当前实例
+
+ */
+
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { getCurrentInstance, onMounted } from "vue";
+
+export default {
+  name: 'GetCurrentInstance',
+  data(){
+    return{
+      x: 1,
+      y: 2
+    }
+  },
+  setup () {
+    console.log('this',this);
+    const instance = getCurrentInstance()
+    
+    onMounted(()=>{
+      console.log('instance x',instance.data.x);
+    })
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
